bfec92190250d6f8885fe31ed62b29df8e3b706b
SanjivRanjan123 <84135735+SanjivRanjan123@users.noreply.github.com>
2021-08-09 15:01:46 +0000

    Revert "Add files via upload"

--- variables.tf
+++ variables.tf
@@ -1,23 +1,25 @@
-#Variable
-# Service Principal Variables
+#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*
+# Linux VM - Variables
+#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*
 
+# Service Principal Variables
 
 variable "client_id" {
     description =   "Client ID (APP ID) of the application"
     type        =   string
-    default     =   "7ea6510f-d934-4a91-9054-f49e3af4d9a3"
+    default     =   "086f9f46-944c-46c0-93db-2241b73047f7"
 }
 
 variable "client_secret" {
     description =   "Client Secret (Password) of the application"
     type        =   string
-    default     =   "-.Nf_q9F1-fMDbN7d1ck7Y7EA.2.jcXX3i"
+    default     =   "YU8As1~o4JPwceH_0NwsM94UL.Z.o3cxgO"
 }
 
 variable "subscription_id" {
     description =  "Subscription ID"
     type        =   string
-    default     =   "b9a6d48b-4f2f-4e69-9e09-9509d0128548"
+    default     =   "8de087f2-1ef1-48c0-abf2-1f7395dd4523"
 }
 
 variable "tenant_id" {
@@ -26,6 +28,24 @@ variable "tenant_id" {
     default     =   "052c7909-2d11-43ce-a9e6-c9343d28fbd4"
 }
 
+# Prefix and Tags
+
+variable "prefix" {
+    description =   "Prefix to append to all resource names"
+    type        =   string
+    default     =   "Proj1"
+}
+
+variable "tags" {
+    description =   "Resouce tags"
+    type        =   map(string)
+    default     =   {
+        "project"       =   "Proj1"
+        "deployed_with" =   "Terraform1"
+    }
+}
+
+# Resource Group
 
 variable "location" {
     description =   "Location of the resource group"
@@ -33,31 +53,85 @@ variable "location" {
     default     =   "East US"
 }
 
-variable "cluster_name" {
-	description =   "cluster name"
+# Vnet and Subnet
+
+variable "vnet_address_range" {
+    description =   "IP Range of the virtual network"
     type        =   string
-    default     =   "proj2myaks"
+    default     =   "10.0.0.0/16"
 }
 
-variable "acr_name" {
-	description =   "Azure Content registry name"
+variable "subnet_address_range" {
+    description =   "IP Range of the virtual network"
     type        =   string
-    default     =   "proj2myacr"
+    default     =   "10.0.1.0/24"
 }
 
-variable "system_node_count" {
-	description =   "Number of AKS worker count"
+# Public IP and NIC Allocation Method
+
+variable "allocation_method" {
+    description =   "Allocation method for Public IP Address and NIC Private ip address"
+    type        =   list(string)
+    default     =   ["Static", "Dynamic"]
+}
+
+
+# VM 
+
+variable "virtual_machine_size" {
+    description =   "Size of the VM"
+    type        =   string
+    default     =   "Standard_D2s_v3"
+}
+
+variable "node_count" {
+    description =   "Count of Linux VMs"
     type        =   number
     default     =   2
 }
 
-
-variable "resource_group_name" {
-	description =   "Azure resource group name"
+variable "computer_name" {
+    description =   "Computer name"
     type        =   string
-    default     =   "proj2_acraks_newrg"
-
+    default     =   "Linuxvm"
 }
 
+variable "admin_username" {
+    description =   "Username to login to the VM"
+    type        =   string
+    default     =   "vmadmin"
+}
 
+variable "admin_password" {
+    description =   "Password to login to the VM"
+    type        =   string
+    default     =   "VMVMAdmin123#"
+}
 
+variable "os_disk_caching" {
+    default     =       "ReadWrite"
+}
+
+variable "os_disk_storage_account_type" {
+    default     =       "StandardSSD_LRS"
+}
+
+variable "os_disk_size_gb" {
+    default     =       64
+}
+
+variable "publisher" {
+    default     =       "Canonical"
+}
+
+variable "offer" {
+    default     =       "UbuntuServer"
+}
+
+variable "sku" {
+    default     =       "18.04-LTS"
+}
+
+variable "vm_image_version" {
+    default     =       "latest"
+}
